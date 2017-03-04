ce4d705677780e10843f14d1dc8934b94d4a8e9e
gao <gaomengnan@rockerfm.com>
2017-03-04 11:58:41 +0800

    redis

--- app.py
+++ app.py
@@ -2,24 +2,24 @@
 from flask import Flask,render_template
 from http import bannerController
 import json
-import redis
+import redisConnect.redisObj
 application = Flask(__name__)
 @application.route("/")
 def index():
-	r = redis.Redis()
-	resp = r.get("banner")
-	if not resp:
-		resp = banner()
-	else:
-		resp  = r.get("banner")
+	resp = banner()
 	return render_template('main.html',data=resp)
 @application.route("/main")
 def main():
     return render_template('main.html')
 
 def banner():
-	b = bannerController.bannerController()
-	resp = b.getBanner()
+	r = redisConnect.redisObj.redisObj()
+	resp = r.get("banner")
+	if not resp:
+		b = bannerController.bannerController()
+		resp = b.getBanner()
+		r.set("banner",resp)
+
 	return json.loads(resp)
 
 if __name__ == "__main__":
---
+++ redisConnect/redisObj.py
@@ -0,0 +1,22 @@
+import redis
+
+
+class redisObj():
+
+    redis_conn = None
+
+    def __init__(self):
+        self.connect()
+
+
+    def connect(self):
+        pool = redis.ConnectionPool()
+        robj = redis.Redis(connection_pool=pool)
+        self.redis_conn = robj
+
+    def get(self,name):
+        return self.redis_conn.get(name)
+
+    def set(self,name,value,ex=3600):
+        self.redis_conn.set(name=name,value=value,ex=ex)
+
