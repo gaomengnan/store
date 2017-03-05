947f49868f8b00e9c21282a93f71d44d67b27e79
gao <gaomengnan@rockerfm.com>
2017-03-05 18:08:52 +0800

    xiugai

--- models/__init__.py
+++ models/__init__.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker,relationships
 
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("mysql://root:@localhost:3306/app?charset=utf8",convert_unicode = True)
+engine = create_engine("mysql://root:@localhost:3306/app?charset=utf8")
 
 
 dbSession = scoped_session(sessionmaker(autocommit=False,
