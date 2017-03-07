85e744d36ca8c0afa97c5f9a7d2e106fa08693a9
gao <gaomengnan@rockerfm.com>
2017-03-07 22:52:09 +0800

    =添加

--- crawlerPosts.py
+++ crawlerPosts.py
@@ -15,19 +15,22 @@ def crawler():
     soup =  BeautifulSoup(resp.text,"html5lib")
     div = soup.find(id="m_86988")
     cols = div.findAll("div",attrs={"class":"yk-col4"})
-
     for col in cols:
-        # print col
-        link = col.find("div",attrs={"class":"v-link"}).a['href']
-        # print link
-        cover = col.div.div.img["_src"]
-        title = col.find("div",attrs={"class":"v-meta-title"}).text
-        has = Posts.Posts.query.filter(Posts.Posts.title == title).first()
-        if not has:
-            new_data = Posts.Posts(title,cover,"优酷",time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time())),link,"院线热映")
-            dbSession.add(new_data)
-            dbSession.commit()
-            dbSession.close()
+
+        is_hovers = col.findAll("div",attrs={"class":"v ishover"})
+        for is_hover in is_hovers:
+            # print col
+            link = is_hover.find("div",attrs={"class":"v-link"}).a['href']
+            # print link
+            cover = is_hover.div.img["_src"]
+            title = is_hover.find("div",attrs={"class":"v-meta-title"}).text
+            has = Posts.Posts.query.filter(Posts.Posts.title == title).first()
+            if not has:
+                new_data = Posts.Posts(title,cover,"优酷",time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time())),link,"院线热映")
+                dbSession.add(new_data)
+                dbSession.commit()
+                dbSession.close()
+                time.sleep(0.5)
 
 
 nameMap = {
