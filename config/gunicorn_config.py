ab42e84be0d0495641d662a0fde3fb07ac7a6966
gaomengnan <gaomengnan@rockerfm.com>
2017-03-10 11:32:41 +0800

    修改模式

--- config/gunicorn_config.py
+++ config/gunicorn_config.py
@@ -6,7 +6,7 @@ import multiprocessing
 workers = multiprocessing.cpu_count()*2+1
 timeout = 30
 graceful_timeout = 30
-worker_class='sync'
+worker_class='gevent'
 loglevel = 'info'
 debug = True
 bind = "127.0.0.1:8080" 
